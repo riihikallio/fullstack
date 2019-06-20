c4f1697a1cc3aa4f0ca4d17f848f877ebb962a5f
Petri Riihikallio <petri.riihikallio@metis.fi>
2019-06-20 21:28:35 +0300

    2.18

---
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+  return axios.get(baseUrl)
+    .then(response => response.data)
+}
+
+const create = person => {
+  return axios.post(baseUrl, person)
+    .then(response => response.data)
+}
+
+const update = (person) => {
+  return axios.put(`${baseUrl}/${person.id}`, person)
+    .then(response => response.data)
+}
+
+const remove = (id) => {
+    return axios.delete(`${baseUrl}/${id}`)
+  }
+
+export default {getAll, create, update, remove}
