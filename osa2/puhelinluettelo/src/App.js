758f46e61110cedc112f6cd6f6e8f804c0a182d7
Petri Riihikallio <petri.riihikallio@metis.fi>
2019-06-23 17:49:42 +0300

    2.20

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import personService from './services/persons'
+import './index.css'
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
 
 const Filter = ({filter, handler}) => (
   <p>Filter shown with <input 
@@ -37,11 +50,20 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const setError = (msg) => {
+    setErrorMessage(msg)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
 
   useEffect(() => {
     personService
       .getAll()
         .then(initial => setPersons(initial))
+        .catch(setError('Retrieving list...'))
   }, [])
 
   const addPerson = (event) => {
@@ -59,7 +81,9 @@ const App = () => {
           setPersons(persons.concat(returned))
           setNewName('')
           setNewNumber('')
+          setError(`'${person.name}' added`)
         })
+        .catch(setError(`Error adding '${person.name}'`))
     } else {
       if(window.confirm(`${newName} is already added to phonebook, replace the old phone number with the new one?`)) {
         const replacing = {...existing, number: newNumber}
@@ -71,7 +95,9 @@ const App = () => {
             setPersons(newList)
             setNewName('')
             setNewNumber('')
+            setError(`'${replacing.name}' edited`)
           })
+          .catch(setError(`Error adding '${replacing.name}'`))
       }
     }
   }
@@ -88,22 +114,26 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  const handleDelete = (person) => {
+  const deletePerson = (person) => {
     if (window.confirm(`Delete ${person.name}`)) {
       personService
         .remove(person.id)
-        .then(whatever => setPersons(persons.filter(p => p.id !== person.id))
-          )
+        .then(whatever => {
+          setPersons(persons.filter(p => p.id !== person.id))
+          setError(`'${person.name}' was deleted`)
+        })
+        .catch(setError(`Error deleting '${person.name}'`))
     }
   }
 
   const rows = () =>  persons
     .filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
-    .map(p => <div key={p.name}>{p.name} {p.number} <button onClick={() => handleDelete(p)}>Delete</button></div>)
+    .map(p => <div key={p.name}>{p.name} {p.number} <button onClick={() => deletePerson(p)}>Delete</button></div>)
 
   return (
     <div>
       <h1>Phonebook</h1>
+      <Notification message={errorMessage} />
       <h2>Filter</h2>
       <Filter 
         filter={filter}
